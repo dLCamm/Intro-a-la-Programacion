cdc950a7c405fb4388a5e52640d6361011fa951a
dLCamm <124687993+dLCamm@users.noreply.github.com>
2024-05-08 09:09:02 -0600

    Agregue los datos de la U a main desde la rama develop

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-print('''Suma de dos numeros
-      a = 5
-      b = 3''')
-a = 5
-b = 3
 
-print(f"el resultado es {a+b}")
 
 
 print('''Nombre: Camilo Efrain de León Sanic
 Carnet: 1612823''')
+
+print('''Universidad Rafael Landivar (URL)
+Campus de Quetzaltenango
+Dirección: 14ave. 0-43, zona 3, Quetzaltenango.
+PBX: 7722 9900 o clic a nuestro WhatsApp
+Correo electrónico: infolandivar@url.edu.gt
+Facebook/ urlcampusdequetzaltenango''')
